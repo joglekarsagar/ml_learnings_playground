1fdaf47fb6281f56fb20397a20a346b68dce8d4b
EC2 Default User <ec2-user@ip-172-16-3-28.ec2.internal>
2023-12-07 17:11:32 +0000

    Add progress with larger model and reversal perforance measurement

--- notebooks/train_config.py
+++ notebooks/train_config.py
@@ -9,27 +9,29 @@ wandb_project = 'gpt_train_string_reversal'
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 batch_size = 16
-block_size = 20
+block_size = 64
 gradient_accumulation_steps = 5 * 8
 
+
 # this makes total number of tokens be 300B
-max_iters = 600000
-lr_decay_iters = 600000
+max_iters = 50000
+lr_decay_iters = 40000
 
 # eval stuff
 eval_interval = 1000
 eval_iters = 200
-log_interval = 10
+log_interval = 500
 
 # weight decay
 weight_decay = 1e-1
 
 # Model stuff
-n_layer = 2
+n_embd = 512
 n_head = 4
-n_embd = 32
+n_layer = 4
 
 learning_rate = 1e-4 # max learning rate
+min_lr = 1e-6 # learning_rate / 10 usually
 
 max_iters = 600000 # total number of training iterations
 
@@ -42,4 +44,6 @@ lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
 
 
 bias=False  
-dropout=True
+dropout=0.1
+
+out_dir = "./checkpoints/"
